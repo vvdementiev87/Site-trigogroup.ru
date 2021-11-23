c885dd8268a674d9eed4418f66624c2b90bfd46a
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2021-11-23 23:47:17 +0300

    update#19 23.11.2021

--- JS/buttonClick.js
+++ JS/buttonClick.js
@@ -23,3 +23,16 @@ for (let element of button3) {
         nav3.classList.toggle('close');
     })
 }
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this.responseText);
+    }
+}
+
+xhttp.open('POST', 'https://trigogroup.ru/login.php', true);
+xhttp.send();
+
+function myFunction(data) {
+    console.log(data);
+}
---
+++ test.php
@@ -0,0 +1,184 @@
+<?php
+include 'include/controller.php';
+include 'include/changepass.php';
+$session_username = $_SESSION['user_name'];
+$session_role = $_SESSION['role'];
+if(empty($_SESSION['user_name'])){
+    header("location:login.php");
+}                  
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">        
+    <title>Data list</title>    
+</head>
+
+<body>
+    <div class="page__wrap">
+        <div id="logout" class="modal__logout nav-1 close" role="dialog">
+            <div class="modal__logout__wrap">
+                <div class="modal__body">
+                    <div class="modal__header">
+                        <h4 class="modal__text">Logout</h4>
+                        <button class="modal__text modal__text--btn btn-1">&times;</button>
+                    </div>
+                    <div class="modal__main">
+                        <input type="hidden" name="delete_id" value="<?php echo $id; ?>">
+                        <h5 class="modal__text">Are you Sure you want to logout
+                            <strong>
+                                <?php echo $_SESSION['user_name']; ?>?
+                            </strong>
+                        </h5>
+                    </div>
+                    <div class="modal__footer">
+                        <a href="logout.php">
+                            <button class="modal__btn--accept">YES </button>
+                        </a>
+                        <button class="modal__btn--close btn-1">NO</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="changepass" class="modal__changepass nav-2 close" role="dialog">
+            <div class="modal__changepass__wrap">
+                <div class="modal__body">
+                    <form action="" class="modal__main__form" method="post">
+                        <div class="modal__header">
+                            <h4 class="modal__text">Change Password</h4>
+                            <button class="modal__text modal__text--btn btn-2">&times;</button>
+                        </div>
+                        <div class="modal__main">
+                            <div class="form-group">
+                                <label class="modal__text" for="name">Current:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="current_password" required
+                                        placeholder="Current Password" autofocus autocomplete="off">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="modal__text" for="name">New:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="new_password" required
+                                        placeholder="New Password" autocomplete="off">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="modal__text" for="name">Repeat:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="repeat_password" required
+                                        placeholder="Repeat Password" autocomplete="off">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal__footer">
+                            <button type="submit" class="modal__btn--accept btn-2" name="change_pass">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div id="missionchange" class="modal__missionchange nav-3 close" role="dialog">
+            <div class="modal__changepass__wrap">
+                <div class="modal__body">
+                    <form action="" class="modal__main__form" method="post">
+                        <div class="modal__header">
+                            <h4 class="modal__text">Change Password</h4>
+                            <button class="modal__text modal__text--btn btn-3">&times;</button>
+                        </div>
+                        <div class="modal__main">
+                            <div class="form-group">
+                                <label class="modal__text" for="name">Current:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="current_password" required
+                                        placeholder="Current Password" autofocus autocomplete="off">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="modal__text" for="name">New:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="new_password" required
+                                        placeholder="New Password" autocomplete="off">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="modal__text" for="name">Repeat:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="repeat_password" required
+                                        placeholder="Repeat Password" autocomplete="off">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal__footer">
+                            <button type="submit" class="modal__btn--accept btn-3" name="change_pass">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <header class="header">
+            <div class="container header__wrap">
+                <div class="header__left">
+                    <a href="#"><img class="header__logo" src="images/sticker_vk_spotty_040.png" alt="LOGO"></a>
+                </div>
+                <div class="header__right">
+                    <div class="header__dropdown">
+                        <details>
+                            <summary class="header__btn">
+                                <?php echo $session_username . " ($session_role)"; ?>
+                            </summary>
+                            <ul class="header__dropdown__menu">
+                                <li><a class="header__link btn-1" href="#logout"> Logout</a></li>
+                                <li><a class="header__link btn-4" href="#changepass"> Change Password</a></li>
+                                <li onclick="location.href = 'main_menu.php'"><a class="header__link " href="#">
+                                        Back</a></li>
+                                <li onclick="location.href = 'missions_all.php'"><a class="header__link " href="#"> Show
+                                        all</a></li>
+                            </ul>
+                        </details>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main class="mainmenu--table container">
+            <table class="mainmenu__table" id="excelDataTable">                
+            </table>            
+        </main>
+        <footer class="footer">
+            <section class="container footer__wrap">
+                <div class="footer__left">
+                    <p>© 2021 Vasek property.</p>
+                </div>
+                <div class="footer__right">
+                    
+                </div>
+            </section>
+        </footer>
+
+ <script>
+   let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this.responseText);
+    }
+}
+
+xhttp.open('POST', 'https://trigogroup.ru/include/controller.php', true);
+xhttp.send();
+
+function myFunction(data) {
+    console.log(data);
+}
+    
+ </script>
+ 
+    </div>
+    <script src="JS/buttonClick.js"></script>
+    
+</body>
+
+</html>
